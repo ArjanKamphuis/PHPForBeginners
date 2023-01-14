bea4db6ee1985ad25f4dcbf53a287a98aae08e87
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-01-15 00:59:54 +0100

    025 - Intro to Forms and Request Methods

--- router.php
+++ router.php
@@ -1,13 +1,5 @@
 <?php
 
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php'
-];
-
 function routeToController(string $uri, array $routes): void
 {
     if (array_key_exists($uri, $routes)) {
@@ -25,4 +17,5 @@ function abort(int $code = Response::NOT_FOUND): never
 }
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$routes = require 'routes.php';
 routeToController($uri, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -1,7 +1,7 @@
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/nav.php'; ?>
 <?php require 'partials/banner.php'; ?>
-        
+
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <ul>
@@ -9,6 +9,9 @@
                 <li><a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= $note['body'] ?></a></li>
             <?php endforeach; ?>
         </ul>
+        <p class="mt-6">
+            <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
+        </p>
     </div>
 </main>
 
