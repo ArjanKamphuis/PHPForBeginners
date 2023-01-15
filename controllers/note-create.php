63d64374b8f97448ccb2289ffb04779209fe8860
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-01-15 01:22:56 +0100

    026 - Always Escape Untrusted Input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,7 +1,13 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $config = require 'config.php';
+    $db = new Database($config['database']);
+
+    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+        ':body' => $_POST['body'],
+        ':user_id' => 1
+    ]);
 }
 
 $heading = 'Create Note';
--- views/notes.view.php
+++ views/notes.view.php
@@ -6,7 +6,11 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <ul>
             <?php foreach ($notes as $note): ?>
-                <li><a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= $note['body'] ?></a></li>
+                <li>
+                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                        <?= htmlspecialchars($note['body']) ?>
+                    </a>
+                </li>
             <?php endforeach; ?>
         </ul>
         <p class="mt-6">
