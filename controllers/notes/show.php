31a8716812428ef782c3ea497b8567bbde226d37
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-01-16 12:19:27 +0100

    032 - Handle Multiple Request Methods From a Controller Action?

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,12 +5,21 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
+$id = $_POST['id'] ?? $_GET['id'] ?? abort();
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    ':id' => $_GET['id']
+    ':id' => $id
 ])->findOrFail();
 
 $currentUserId = 1;
 authorize($note['user_id'] === $currentUserId);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->query('DELETE from notes WHERE id = :id', [
+        ':id' => $id
+    ]);
+    header('Location: /notes');
+    exit();
+}
 
 view('notes.show', [
     'heading' => 'Note',
