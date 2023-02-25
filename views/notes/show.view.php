a10aafd2328ffbf54388bcc1352f7dd6167f0e85
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-02-26 00:04:09 +0100

    Button equality

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -4,11 +4,9 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p class="mb-6">
-            <a href="/notes" class="text-blue-500 hover:underline">Go back...</a>
-        </p>
         <p><?= htmlspecialchars($note['body']) ?></p>
         <footer class="mt-6">
+            <a href="/notes" class="inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">Go back</a>
             <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">Edit</a>
         </footer>
     </div>
