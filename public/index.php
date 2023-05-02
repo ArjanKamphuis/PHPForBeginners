43e1df3154ca6353d4c27e7d5ee0b8fa896fbded
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-05-02 16:26:05 +0200

    048 - Install Two Composer Packages: Collections and PestPHP

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ use Core\Session;
 session_start();
 
 const BASE_PATH = __DIR__ . '/../';
+
 require BASE_PATH . 'vendor/autoload.php';
 require BASE_PATH . 'Core/functions.php';
-
-require base_path('bootstrap.php');
+require BASE_PATH . 'bootstrap.php';
 
 $router = new Router();
-$routes = require base_path('routes.php');
+require BASE_PATH . 'routes.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
